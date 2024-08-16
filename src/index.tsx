a3350cf1e632ff97092db96ae4e4946c711ac52e
Dmitry Lyapin <lyapindm@yandex.ru>
2024-08-16 13:26:14 +0300

    Подключил store

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
+import { App } from './app';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
 
